c44e18be67790f641022f6d53fb71d3ad6eb3dee
unknown <2511500080@mahasiswa.atmaluhur.ac.id>
2025-10-27 15:17:10 +0700

    membuat script.js

--- Pertemuan-05/script.js
+++ Pertemuan-05/script.js
@@ -1,3 +1,3 @@
 document.getElementById("menuToggle").addEventListener("click", function() {
-    document.querySelector("nav ul").classList.toggle("active");
+    document.querySelector("nav").classList.toggle("active");
 });
